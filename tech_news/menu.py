73316a2cf420eca895147c66334288b6c70764d4
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-02-10 19:53:13 -0300

    feat: menu with functions

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,21 +1,32 @@
 # Requisitos 11 e 12
 import sys
+from tech_news.scraper import get_tech_news
+from tech_news.analyzer.ratings import top_5_categories
+from tech_news.analyzer.search_engine import (
+    search_by_category,
+    search_by_date,
+    search_by_title,
+)
 
 
 def analyzer_menu():
-    sys.stdout.write(
+    option = input(
         "Selecione uma das opções a seguir:\n 0 - Popular o banco com notícias"
         ";\n 1 - Buscar notícias por título;\n 2 - Buscar notícias por data;\n"
         " 3 - Buscar notícias por categoria;\n "
         "4 - Listar top 5 categorias;\n 5 - Sair.\n"
     )
     switcher = {
-        "0": "Digite quantas notícias serão buscadas:\n",
-        "1": "Digite o título:\n",
-        "2": "Digite a data no formato aaaa-mm-dd:\n",
-        "3": "Digite a categoria:\n",
+        "0": lambda: get_tech_news(
+            int(input("Digite quantas notícias serão buscadas: "))
+        ),
+        "1": lambda: search_by_title(input("Digite o título: ")),
+        "2": lambda: search_by_date(input()),
+        "3": lambda: search_by_category(input("Digite a categoria:")),
+        "4": lambda: top_5_categories(),
+        "5": lambda: "Encerrando script",
     }
     try:
-        sys.stdout.write(switcher[input()])
+        print(switcher[option](), file=sys.stdout)
     except KeyError:
         sys.stderr.write("Opção inválida\n")
