10969e2cbc8ae1b15b2654965b6af380bb6f2cb3
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-02-10 11:25:11 -0300

    feat: search by title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,10 @@
 # Requisito 7
-def search_by_title(title):
-    """Seu código deve vir aqui"""
+from tech_news.database import search_news
+
+
+def search_by_title(title: str) -> list[tuple[str, str]]:
+    news_list = search_news({"title": {"$regex": title, "$options": "i"}})
+    return [(news["title"], news["url"]) for news in news_list]
 
 
 # Requisito 8
