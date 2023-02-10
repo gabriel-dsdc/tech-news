b99df451ad7db8c2d207549a41972d88192085e8
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-02-10 16:21:23 -0300

    feat: top five categories ratings

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,13 @@
 # Requisito 10
-def top_5_categories():
-    """Seu código deve vir aqui"""
+from tech_news.database import find_news
+
+
+def top_5_categories() -> list[str]:
+    categories_list = [news["category"] for news in find_news()]
+    categories_count = {}
+    for category in categories_list:
+        categories_count[category] = categories_list.count(category)
+    categories_sorted = sorted(
+        categories_count.items(), key=lambda x: (-x[1], x[0])
+    )
+    return [item[0] for item in categories_sorted[:5]]
