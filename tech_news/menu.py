7ca87de817a6266186f8a3414fd02d9aac545a50
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-02-10 17:40:35 -0300

    feat: implemented basic manu

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,21 @@
 # Requisitos 11 e 12
+import sys
+
+
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    sys.stdout.write(
+        "Selecione uma das opções a seguir:\n 0 - Popular o banco com notícias"
+        ";\n 1 - Buscar notícias por título;\n 2 - Buscar notícias por data;\n"
+        " 3 - Buscar notícias por categoria;\n "
+        "4 - Listar top 5 categorias;\n 5 - Sair.\n"
+    )
+    switcher = {
+        "0": "Digite quantas notícias serão buscadas:\n",
+        "1": "Digite o título:\n",
+        "2": "Digite a data no formato aaaa-mm-dd:\n",
+        "3": "Digite a categoria:\n",
+    }
+    try:
+        sys.stdout.write(switcher[input()])
+    except KeyError:
+        sys.stderr.write("Opção inválida\n")
